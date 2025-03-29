f63304ad8c85dba1ff8e027c90b201f0f34671f1
Blaz <blaz.hercog@gmail.com>
2025-03-29 09:57:53 +0000

    Added struct and print_help

--- src/main.c
+++ src/main.c
@@ -1,7 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int
+struct Struktura {
+    unsigned short index;
+    char code[3];
+    int units;
+    double length;
+};
+
+void print_help(){
+    printf("tableprint\n\n");
+    printf("-h\n Izpis pomoci z zastavicami in njihovimi opisi\n");
+    printf("-i <pot>\n Kot vhod program odpre in uporabi zbirko/napravo s podano potjo\n");
+    printf("-o <pot>\n Kot izhod program odpre in uporabi zbirko/napravo s podano potjo\n");
+}
 main(int argc, char *argv[])
 {
     printf("Hello world!\n");
