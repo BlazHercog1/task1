80e75dcd587ebbf95c5c51fb6e9f19e347b36c10
Blaz <blaz.hercog@gmail.com>
2025-03-29 10:11:03 +0000

    Added print_csv and print_table functions

--- src/main.c
+++ src/main.c
@@ -14,6 +14,15 @@ void print_help(){
     printf("-i <pot>\n Kot vhod program odpre in uporabi zbirko/napravo s podano potjo\n");
     printf("-o <pot>\n Kot izhod program odpre in uporabi zbirko/napravo s podano potjo\n");
 }
+
+void print_csv(struct Struktura str, FILE *out){
+    fprintf(out, "%u, \"%.*s\", %d, %.6f\n", str.index, 3, str.code, str.units, str.length);
+}
+
+void print_table(struct Struktura str, FILE *out){
+    fprintf(out, "%-5u | %-3.3s | %10d | %8.4f\n", str.index, str.code, str.units,str.length);
+}
+
 main(int argc, char *argv[])
 {
     printf("Hello world!\n");
